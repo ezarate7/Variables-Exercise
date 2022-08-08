1a07f2db10c1be3b584d940b2394d1bf74436644
Eddie Z <e.zarate7@hotmail.com>
2022-08-07 18:09:05 -0600

    initial message

--- Variables exercise/Program.cs
+++ Variables exercise/Program.cs
@@ -1,15 +1,16 @@
 ﻿
-Console.WriteLine("Hello, Amoriss");
-string dogName = "Buzz";
-int dogAge = 10;
-Double dogValue = 10.4;
+{
+    Console.WriteLine("Hello, Amoriss");
+    string dogName = "Buzz";
+    int dogAge = 10;
+    Double dogValue = 10.4;
 
 
+    Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
+    Console.WriteLine($"{dogName} weighs {dogValue} pounds");
+    Console.WriteLine("Have a good day!");
+}
 
-
-Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
-Console.WriteLine($"{dogName} weighs {dogValue} pounds");
-Console.WriteLine("Have a good day!");
 
 
 
